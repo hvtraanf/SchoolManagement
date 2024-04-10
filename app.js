6ea141c257ff5d36e6ca952d5e3fc211be06471f
hvtraanf <vhoangtran99@gmail.com>
2024-04-10 16:19:26 +0700

    Update app.js

--- app.js
+++ app.js
@@ -59,6 +59,7 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+  app.listen(process.env.PORT || 3001);
 });
 
 module.exports = app;

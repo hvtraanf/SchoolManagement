20f1111efde9886a8237736d2dde422da392c492
hvtraanf <vhoangtran99@gmail.com>
2024-04-10 16:47:53 +0700

    Update app.js

--- app.js
+++ app.js
@@ -50,6 +50,8 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
+app.listen(process.env.PORT || 3001);
+
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
@@ -59,7 +61,6 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
-  app.listen(process.env.PORT || 3001);
 });
 
 module.exports = app;
